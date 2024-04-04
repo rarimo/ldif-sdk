a829228ddf16ba54f0f5156c4063a3bed268d62c
violog <51th.apprent1ce.f0rce@gmail.com>
2024-04-04 17:51:00 +0300

    Fix hashing public keys, introduce constants and errors

--- utils/cert.go
+++ utils/cert.go
@@ -2,27 +2,28 @@ package utils
 
 import (
 	"crypto/rsa"
+	"fmt"
 
 	"github.com/rarimo/certificate-transparency-go/x509"
 )
 
+// ExtractPubKeys extracts N values of RSA public keys from certificates, which
+// can be used for hashing later.
 func ExtractPubKeys(certs []*x509.Certificate) ([][]byte, error) {
 	pubKeys := make([][]byte, 0, len(certs))
-	pkMap := make(map[string]struct{}, len(certs)-1)
+	pkMap := make(map[string]struct{}, len(certs))
 
 	for _, cert := range certs {
 		key, ok := cert.PublicKey.(*rsa.PublicKey)
 		if !ok {
-			// FIXME @violog Handle ecdsa.PublicKey
+			return nil, fmt.Errorf("%T: %w", cert.PublicKey, ErrUnsupportedPublicKey)
+		}
+
+		if _, ok = pkMap[key.N.String()]; ok {
 			continue
 		}
 
-		// string() conversion is intended: as we only use it as a map key, encoding is not important
-		if _, ok = pkMap[string(key.N.Bytes())]; ok {
-			continue
-		}
-
-		pkMap[string(key.N.Bytes())] = struct{}{}
+		pkMap[key.N.String()] = struct{}{}
 		pubKeys = append(pubKeys, key.N.Bytes())
 	}
 
--- utils/hash.go
+++ utils/hash.go
@@ -11,23 +11,41 @@ import (
 	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
-const chunksAmount = 64
+const (
+	pubKeyLength   = 4096
+	chunksAmount   = 64
+	subChunkLength = 16 // = len(poseidon.NROUNDSP)
+)
 
+var (
+	ErrUnsupportedPublicKey = errors.New("unsupported public key, supported formats: rsa")
+	ErrInvalidLength        = errors.New("input length must be 4096 bits")
+)
+
+// HashCertificate hashes the public key of the certificate, calling PoseidonHash4096
 func HashCertificate(certificate *x509.Certificate) (*big.Int, error) {
-	return HashPublicKey(certificate.RawSubjectPublicKeyInfo)
-}
-
-func HashPublicKey(publicKey []byte) (*big.Int, error) {
-	rsaPK, err := ParseCertPublicKey(publicKey)
-	if err != nil {
-		return nil, fmt.Errorf("parse public key: %w", err)
+	rsaPK, ok := certificate.PublicKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("%T: %w", certificate.PublicKey, ErrUnsupportedPublicKey)
 	}
 
-	splitedPubKey := splitPublicKey(rsaPK)
+	return PoseidonHash4096(rsaPK.N.Bytes())
+}
 
-	hashedChunks := make([]*big.Int, 0, 4)
-	for i := 0; i < len(splitedPubKey); i += 16 {
-		chunks := splitedPubKey[i : i+16]
+// PoseidonHash4096 hashes 4096 bits of raw data with Poseidon hash function,
+// applying splitting data on chunks
+func PoseidonHash4096(raw []byte) (*big.Int, error) {
+	if len(raw) != pubKeyLength {
+		return nil, fmt.Errorf("%w, got %d", ErrInvalidLength, len(raw))
+	}
+
+	// split 4096 bits into 64 chunks of 64 bits each
+	splitedPubKey := splitBytes(raw)
+	hashedChunks := make([]*big.Int, 0, chunksAmount/subChunkLength)
+
+	// on each iteration, hash 16 chunks of 64 bits each
+	for i := 0; i < len(splitedPubKey); i += subChunkLength {
+		chunks := splitedPubKey[i : i+subChunkLength]
 		chunkHash, err := poseidon.Hash(chunks)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to hash poseidon 16", logan.F{"chunks": chunks})
@@ -43,8 +61,10 @@ func HashPublicKey(publicKey []byte) (*big.Int, error) {
 	return chunkHash, nil
 }
 
-func splitPublicKey(rsaPK *rsa.PublicKey) []*big.Int {
-	chunkedPubKey := chunkBytes(rsaPK.N.Bytes(), len(rsaPK.N.Bytes())/chunksAmount)
+// convert bytes from chunkBytes to big integers
+func splitBytes(rsaN []byte) []*big.Int {
+	chunkedPubKey := chunkBytes(rsaN, len(rsaN)/chunksAmount)
+
 	splitedPubKey := make([]*big.Int, len(chunkedPubKey))
 	for i, bytes := range chunkedPubKey {
 		splitedPubKey[i] = new(big.Int).SetBytes(bytes)
@@ -53,7 +73,10 @@ func splitPublicKey(rsaPK *rsa.PublicKey) []*big.Int {
 	return splitedPubKey
 }
 
-func chunkBytes(data []byte, chunkSize int) (chunks [][]byte) {
+// divide slice to chunks of size chunkSize
+func chunkBytes(data []byte, chunkSize int) [][]byte {
+	chunks := make([][]byte, 0, len(data)/chunkSize)
+
 	for chunkSize < len(data) {
 		chunks = append(chunks, data[0:chunkSize])
 		data = data[chunkSize:]
